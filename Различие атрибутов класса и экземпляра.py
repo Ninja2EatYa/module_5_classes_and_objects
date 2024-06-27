5f5514a0488e8c043d03fca50649b8ea7c75c133
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-06-27 23:49:58 +0300

    Различие атрибутов класса и экземпляра.py

--- Различие атрибутов класса и экземпляра.py
+++ Различие атрибутов класса и экземпляра.py
@@ -6,56 +6,12 @@ class House:
             cls.houses_history.append(i)
         return object.__new__(cls)
 
-    def __del__(self):
-        print(f'{self.name} снесён, но он останется в истории')
-
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def go_to(self, new_floor):
-        if new_floor >= self.number_of_floors or new_floor <= 0:
-            print('Такого этажа не существует')
-        else:
-            for i in range(new_floor):
-                print(i + 1)
-        return self.go_to
-
-    def __len__(self):
-        return self.number_of_floors
-
-    def __str__(self):
-        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
-
-    def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
-
-    def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
-
-    def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
-
-    def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
-
-    def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
-
-    def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
-
-    def __add__(self, value):
-        self.number_of_floors += value
-        return self
-
-    def __radd__(self, value):
-        self.number_of_floors += value
-        return self
-
-    def __iadd__(self, value):
-        self.number_of_floors += value
-        return self
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
 
 
 h1 = House('ЖК Эльбрус', 10)
